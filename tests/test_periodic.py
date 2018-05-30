33c9942f3e6adb20dfe024ecdecfc82635fd8831
Dmitry Orlov <me@mosquito.su>
2018-05-30 12:41:51 +0300

    [fix] don't run periodic calls while previous call still running

--- tests/test_periodic.py
+++ tests/test_periodic.py
@@ -23,3 +23,21 @@ async def test_periodic(event_loop):
     await asyncio.sleep(0.5, loop=event_loop)
 
     assert 4 < counter < 7
+
+
+@pytest.mark.asyncio
+async def test_long_func(event_loop):
+    counter = 0
+
+    async def task():
+        nonlocal counter
+        counter += 1
+        await asyncio.sleep(0.5)
+
+    periodic = PeriodicCallback(task)
+    periodic.start(0.1, event_loop)
+
+    await asyncio.sleep(1, loop=event_loop)
+    periodic.stop()
+
+    assert 1 < counter < 3
