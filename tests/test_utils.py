cf3199a952e9d983f5791c526f0cd8f3e6cd0df6
Dmitry Orlov <me@mosquito.su>
2018-07-30 14:32:18 +0300

    wait_for

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,3 +1,4 @@
+import asyncio
 import json
 import logging
 import socket
@@ -6,7 +7,8 @@ import uuid
 
 import pytest
 
-from aiomisc.utils import chunk_list, bind_socket
+from aiomisc.entrypoint import entrypoint
+from aiomisc.utils import chunk_list, bind_socket, wait_for
 from aiomisc.log import basic_config
 
 
@@ -59,3 +61,66 @@ def test_bind_address(address, family, unused_tcp_port):
 
     assert isinstance(sock, socket.socket)
     assert sock.family == family
+
+
+def test_wait_for_dummy():
+    with entrypoint() as loop:
+        results = loop.run_until_complete(
+            wait_for(*[asyncio.sleep(0.1) for _ in range(100)])
+        )
+
+    assert len(results) == 100
+    assert results == [None] * 100
+
+
+def test_wait_for_exception():
+    async def coro(arg):
+        await asyncio.sleep(0.1)
+        assert arg != 15
+        return arg
+
+    with entrypoint() as loop:
+        with pytest.raises(AssertionError):
+            loop.run_until_complete(
+                wait_for(*[coro(i) for i in range(100)])
+            )
+
+        results = loop.run_until_complete(
+            wait_for(
+                *[coro(i) for i in range(17)],
+                raise_first=False
+            ),
+        )
+
+    assert results
+    assert len(results) == 17
+    assert isinstance(results[15], AssertionError)
+    assert results[:15] == [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]
+    assert results[16:] == [16]
+
+
+def test_wait_for_cancelling():
+    results = []
+
+    async def coro(arg):
+        nonlocal results
+        await asyncio.sleep(0.1)
+        assert arg != 15
+
+        if arg > 15:
+            await asyncio.sleep(1)
+
+        results.append(arg)
+
+    with entrypoint() as loop:
+        with pytest.raises(AssertionError):
+            loop.run_until_complete(
+                wait_for(*[coro(i) for i in range(100)])
+            )
+
+        loop.run_until_complete(asyncio.sleep(2))
+
+    assert results
+    assert len(results) == 15
+    assert len(set(results)) == 15
+    assert frozenset(results) == frozenset(range(15))
