b9dcd0741d12dc7492ece4c48331dfde59788e4b
Dmitry Orlov <me@mosquito.su>
2018-09-13 18:31:18 +0300

    add memory tacer service

--- aiomisc/service/__init__.py
+++ aiomisc/service/__init__.py
@@ -1,9 +1,11 @@
 from .base import Service, ServiceMeta, SimpleServer
 from .tcp import TCPServer
 from .udp import UDPServer
+from .tracer import MemoryTracer
 
 
 __all__ = (
+    'MemoryTracer',
     'Service',
     'ServiceMeta',
     'SimpleServer',
