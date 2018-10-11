3afb57888d0e99647cda3c3db17fabcaa48c14fe
Dmitry Orlov <me@mosquito.su>
2018-10-11 12:54:34 +0300

    log when periodic callback errored

--- tests/test_periodic.py
+++ tests/test_periodic.py
@@ -41,3 +41,35 @@ async def test_long_func(event_loop):
     periodic.stop()
 
     assert 1 < counter < 3
+
+
+@pytest.mark.asyncio
+async def test_shield(event_loop):
+    counter = 0
+
+    async def task():
+        nonlocal counter
+        counter += 1
+        await asyncio.sleep(2)
+
+    periodic = PeriodicCallback(task)
+    periodic.start(0.1, event_loop, shield=True)
+
+    await asyncio.sleep(0)
+
+    with pytest.raises(asyncio.CancelledError):
+        await periodic.stop()
+
+    assert counter == 1
+
+    # No shield
+    counter = 0
+    periodic = PeriodicCallback(task)
+    periodic.start(0.1, event_loop, shield=False)
+
+    await asyncio.sleep(0)
+
+    with pytest.raises(asyncio.CancelledError):
+        await periodic.stop()
+
+    assert counter == 0
