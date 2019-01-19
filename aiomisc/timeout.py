9461fbf96ea3e35aa6322544e1c4b7e63d180106
Dmitry Orlov <me@mosquito.su>
2019-01-19 20:37:04 +0300

    [fix] cancel timeout handle when success

--- aiomisc/timeout.py
+++ aiomisc/timeout.py
@@ -1,6 +1,5 @@
 import asyncio
 from functools import wraps
-from time import monotonic
 from typing import Union, TypeVar
 
 
@@ -16,43 +15,15 @@ class timeout:
         if not asyncio.iscoroutinefunction(func):
             raise TypeError("Function is not a coroutine function")
 
-        async def awaiter(task):
-            await task
-
-        def cancel(loop, task: asyncio.Task, expired: asyncio.Event = None):
-            if task.done():
-                return
-
-            task.cancel()
-
-            if expired:
-                expired.set()
-
-            return loop.create_task(awaiter(task))
-
         @wraps(func)
         async def wrap(*args, **kwargs):
-            try:
-                loop = asyncio.get_event_loop()
-                expired = asyncio.Event(loop=loop)
+            loop = asyncio.get_event_loop()
 
-                # noinspection PyCallingNonCallable
-                task = loop.create_task(
-                    func(*args, **kwargs)
-                )   # type: asyncio.Task
-
-                loop.call_later(self.timeout, cancel, loop, task, expired)
-
-                try:
-                    return await task
-                except asyncio.CancelledError as e:
-                    cancel(loop, task, None)
-                    if expired.is_set():
-                        raise asyncio.TimeoutError from e
-                    raise
-
-            except Exception as e:
-                print(monotonic(), 'inner', repr(e))
-                raise
+            # noinspection PyCallingNonCallable
+            return await asyncio.wait_for(
+                func(*args, **kwargs),
+                self.timeout,
+                loop=loop
+            )   # type: asyncio.Task
 
         return wrap
