33a60a742dddda87ec6e0eb639d8de234d807fc3
IURII Shikanov <dizballanze@gmail.com>
2019-05-22 18:14:15 +0300

    Type annotation in comments for py35 support

--- aiomisc/service/periodic.py
+++ aiomisc/service/periodic.py
@@ -10,7 +10,7 @@ class PeriodicService(Service):
 
     __required__ = ('interval',)
 
-    interval: float  # in seconds
+    interval = None  # type: float # in seconds
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
