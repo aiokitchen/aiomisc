a8d2c9f57683195f86f762eb0f3c2fac25d44f30
Artyom Nikitin <a.nikitin@edadeal.ru>
2019-10-25 13:22:18 +0300

    test: periodic delay

--- tests/test_periodic.py
+++ tests/test_periodic.py
@@ -73,3 +73,24 @@ async def test_shield(loop):
         await periodic.stop()
 
     assert counter == 0
+
+
+async def test_delay(loop):
+    counter = 0
+
+    def task():
+        nonlocal counter
+        counter += 1
+
+    periodic = aiomisc.PeriodicCallback(task)
+    periodic.start(0.1, loop, delay=0.5)
+
+    await asyncio.sleep(0.25, loop=loop)
+
+    assert not counter
+
+    await asyncio.sleep(0.5, loop=loop)
+
+    periodic.stop()
+
+    assert 1 < counter < 4
--- tests/test_periodic_service.py
+++ tests/test_periodic_service.py
@@ -12,8 +12,8 @@ def test_str_representation():
     class FooPeriodicService(PeriodicService):
         ...
 
-    svc = FooPeriodicService(interval=42)
-    assert str(svc) == 'FooPeriodicService(interval=42)'
+    svc = FooPeriodicService(interval=42, delay=4815162342)
+    assert str(svc) == 'FooPeriodicService(interval=42,delay=4815162342)'
 
 
 def test_periodic():
@@ -38,6 +38,34 @@ def test_periodic():
 
         await asyncio.sleep(0.5)
         assert 4 <= counter <= 7
+
+    with aiomisc.entrypoint(svc) as loop:
+        loop.run_until_complete(assert_counter())
+
+
+def test_delay():
+    counter = 0
+
+    class CountPeriodicService(PeriodicService):
+        async def callback(self):
+            nonlocal counter
+            counter += 1
+            await asyncio.sleep(0)
+
+    svc = CountPeriodicService(interval=0.1, delay=0.5)
+
+    async def assert_counter():
+        nonlocal counter, svc
+
+        counter = 0
+        await asyncio.sleep(0.25)
+        assert not counter
+
+        await asyncio.sleep(0.5)
+
+        await svc.stop(None)
+
+        assert 1 < counter < 4
 
     with aiomisc.entrypoint(svc) as loop:
         loop.run_until_complete(assert_counter())
