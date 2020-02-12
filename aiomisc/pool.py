9e644433dffc942e2ddebb19ae87b8efdb6653c1
Orlov Dmitry <mosquito@yandex-team.ru>
2020-02-13 02:17:12 +0300

    [fix] use parent from typing

--- aiomisc/pool.py
+++ aiomisc/pool.py
@@ -1,16 +1,11 @@
 import asyncio
 from abc import abstractmethod, ABC
+from typing import AsyncContextManager
 
 from .utils import cancel_tasks
 
 
-try:
-    from contextlib import AbstractAsyncContextManager
-except ImportError:
-    from typing import AsyncContextManager as AbstractAsyncContextManager
-
-
-class ContextManager(AbstractAsyncContextManager):
+class ContextManager(AsyncContextManager):
     __slots__ = "__aenter", "__aexit", "__instance"
 
     sentinel = object()
