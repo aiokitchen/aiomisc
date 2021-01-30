b260b5bb0772a596461c714ecfdffa89df82b49d
Artyom Nikitin <a.nikitin@edadeal.ru>
2021-01-30 03:17:12 +0300

    test: add more control

--- tests/test_aggregate.py
+++ tests/test_aggregate.py
@@ -1,5 +1,6 @@
 import asyncio
 import math
+from asyncio import Event
 from time import monotonic
 from typing import List
 
@@ -40,10 +41,13 @@ async def test_invalid_max_count(max_count):
 
 
 async def test_error(loop):
+    event = Event()
+    jitter = 0.5
     leeway = 0.01
 
     @aggregate(leeway * 1000)
     async def pow(*args: float, power: float = 2) -> List[float]:
+        event.set()
         raise ValueError
 
     async def pho(num: int):
@@ -53,7 +57,11 @@ async def test_error(loop):
     for i in range(10):
         tasks.append(loop.create_task(pho(i)))
 
-    await asyncio.sleep(leeway * 1.5)
+    t = monotonic()
+    await event.wait()
+    elapsed = monotonic() - t
+    assert leeway < elapsed < leeway * (1 + jitter)
+    await asyncio.sleep(leeway * 0.1)
 
     assert tasks
     for task in tasks:
@@ -62,37 +70,53 @@ async def test_error(loop):
 
 
 async def test_leeway_ok(loop):
+    event = Event()
+    jitter = 0.5
     leeway = 0.1
 
     @aggregate(leeway * 1000)
     async def pow(*args: float, power: float = 2) -> List[float]:
+        event.set()
         return [math.pow(num, power) for num in args]
 
     tasks = []
     for i in range(9):
         tasks.append(loop.create_task(pow(i)))
-        await asyncio.sleep(leeway / 10 * 0.9)
+    t = monotonic()
+    await asyncio.sleep(leeway * (1 - jitter))
+    elapsed = monotonic() - t
+    assert 0 < elapsed < leeway
     assert all(not task.done() for task in tasks)
 
-    await asyncio.sleep(leeway * 2 / 10)
+    await event.wait()
+    elapsed = monotonic() - t
+    assert leeway < elapsed < leeway * (1 + jitter)
+    await asyncio.sleep(leeway * 0.1)
     for i, task in enumerate(tasks):
         assert task.done()
         assert task.result() == math.pow(i, 2)
 
 
 async def test_max_count(loop):
+    event = Event()
+    jitter = 0.5
     leeway = 0.1
     max_count = 5
 
     @aggregate(leeway * 1000, max_count)
     async def pow(*args: float, power: float = 2) -> List[float]:
+        event.set()
         return [math.pow(num, power) for num in args]
 
     tasks = []
     for i in range(5):
         tasks.append(loop.create_task(pow(i)))
 
-    await asyncio.sleep(leeway / 10)
+    t = monotonic()
+    await event.wait()
+    elapsed = monotonic() - t
+    assert 0 < elapsed < leeway * (1 - jitter)
+    await asyncio.sleep(leeway * 0.1)
 
     for i, task in enumerate(tasks):
         assert tasks[i].done()
@@ -100,30 +124,47 @@ async def test_max_count(loop):
 
 
 async def test_max_count_multiple_batches(loop):
+    event = Event()
+    jitter = 0.5
     leeway = 0.1
     max_count = 5
 
     @aggregate(leeway * 1000, max_count)
     async def pow(*args: float, power: float = 2) -> List[float]:
+        event.set()
         return [math.pow(num, power) for num in args]
 
     tasks = []
     for i in range(9):
         tasks.append(loop.create_task(pow(i)))
-        await asyncio.sleep(leeway / 10)
+
+    # Wait for the first batch
+    t = monotonic()
+    await event.wait()
+    event.clear()
+    elapsed = monotonic() - t
+    assert 0 < elapsed < leeway * (1 - jitter)
+    await asyncio.sleep(leeway * 0.1)
 
     for i in range(5):
         assert tasks[i].done()
     for i in range(5, 9):
         assert not tasks[i].done()
 
-    await asyncio.sleep(leeway * 7 / 10)
+    # Wait for the second batch
+    await event.wait()
+    elapsed = monotonic() - t
+    assert 0 < elapsed < leeway * (1 + jitter)
+    await asyncio.sleep(leeway * 0.1)
+
     for i, task in enumerate(tasks):
         assert task.done()
         assert task.result() == math.pow(i, 2)
 
 
 async def test_leeway_cancel(loop):
+    event = Event()
+    jitter = 0.5
     executions = 0
     leeway = 0.1
     arg = ContextVar('arg')
@@ -135,6 +176,7 @@ async def test_leeway_cancel(loop):
         nonlocal executions, executing_task
         executions += 1
         executing_task = tasks[arg.get()]
+        event.set()
         await asyncio.sleep(leeway)
         return [math.pow(num, power) for num in args]
 
@@ -144,18 +186,19 @@ async def test_leeway_cancel(loop):
 
     for i in range(9):
         tasks.append(loop.create_task(pho(i)))
-        await asyncio.sleep(leeway / 10 * 0.95)
-
-    assert all(not task.done() for task in tasks)
 
     # Execution must have started
-    await asyncio.sleep(leeway / 10)
+    t = monotonic()
+    await event.wait()
+    event.clear()
+    elapsed = monotonic() - t
+    assert leeway < elapsed < leeway * (1 + jitter)
     assert executions == 1
     first_executing_task = executing_task
     first_executing_task.cancel()
 
     # Another task must have tried to execute
-    await asyncio.sleep(leeway / 10)
+    await event.wait()
     assert executions == 2
     assert first_executing_task.cancelled()
     assert all(
@@ -164,7 +207,7 @@ async def test_leeway_cancel(loop):
     )
 
     # Must have finished
-    await asyncio.sleep(leeway)
+    await asyncio.sleep(leeway * 1.1)
     assert first_executing_task.cancelled()
     for i, task in enumerate(tasks):
         if task is first_executing_task:
@@ -174,6 +217,8 @@ async def test_leeway_cancel(loop):
 
 
 async def test_max_count_cancel(loop):
+    event = Event()
+    jitter = 0.5
     executions = 0
     leeway = 100
     max_count = 5
@@ -186,6 +231,7 @@ async def test_max_count_cancel(loop):
         nonlocal executions, executing_task
         executions += 1
         executing_task = tasks[arg.get()]
+        event.set()
         await asyncio.sleep(0.1)
         return [math.pow(num, power) for num in args]
 
@@ -197,16 +243,19 @@ async def test_max_count_cancel(loop):
     for i in range(5):
         tasks.append(loop.create_task(pho(i)))
 
-    await asyncio.sleep(0.01)
-
     # Execution must have started
+    t = monotonic()
+    await event.wait()
+    event.clear()
+    elapsed = monotonic() - t
+    assert 0 < elapsed < leeway * (1 - jitter)
     assert all(not task.done() for task in tasks)
     assert executions == 1
     first_executing_task = executing_task
     first_executing_task.cancel()
 
     # Another task must have tried to execute
-    await asyncio.sleep(0.01)
+    await event.wait()
     assert executions == 2
     assert first_executing_task.cancelled()
     assert all(
@@ -215,7 +264,7 @@ async def test_max_count_cancel(loop):
     )
 
     # Must have finished
-    await asyncio.sleep(0.1)
+    await asyncio.sleep(0.11)
     assert first_executing_task.cancelled()
     for i, task in enumerate(tasks):
         if task is first_executing_task:
@@ -225,6 +274,7 @@ async def test_max_count_cancel(loop):
 
 
 async def test_max_count_multiple_batches_cancel(loop):
+    event = Event()
     executions = 0
     leeway = 0.1
     max_count = 5
@@ -237,6 +287,7 @@ async def test_max_count_multiple_batches_cancel(loop):
         nonlocal executions, executing_task
         executions += 1
         executing_task = tasks[arg.get()]
+        event.set()
         await asyncio.sleep(leeway)
         return [math.pow(num, power) for num in args]
 
@@ -247,16 +298,20 @@ async def test_max_count_multiple_batches_cancel(loop):
     tasks = []
     for i in range(9):
         tasks.append(loop.create_task(pho(i)))
-        await asyncio.sleep(leeway / 10)
 
     # Execution of the first batch must have started
+    await event.wait()
+    event.clear()
+    await asyncio.sleep(leeway * 0.1)
+
     assert all(not task.done() for task in tasks)
     assert executions == 1
     first_executing_task = executing_task
     first_executing_task.cancel()
 
     # Another task must have tried to execute
-    await asyncio.sleep(leeway / 10)
+    await event.wait()
+    event.clear()
     assert executions == 2
     assert first_executing_task.cancelled()
     assert all(
@@ -265,7 +320,7 @@ async def test_max_count_multiple_batches_cancel(loop):
     )
 
     # First batch must have finished
-    await asyncio.sleep(leeway)
+    await asyncio.sleep(leeway * 1.1)
     assert first_executing_task.cancelled()
     for i, task in enumerate(tasks[:5]):
         if task is first_executing_task:
@@ -274,11 +329,12 @@ async def test_max_count_multiple_batches_cancel(loop):
         assert task.result() == math.pow(i, 2)
 
     # Second batch must have started execution
+    await event.wait()
     assert all(not task.done() for task in tasks[5:])
     assert executions == 3
 
     # Second batch mast have finished
-    await asyncio.sleep(leeway)
+    await asyncio.sleep(leeway * 1.1)
     for i, task in enumerate(tasks[5:], start=5):
         assert task.done()
         assert task.result() == math.pow(i, 2)
