c9dddde4913dc6fc55836bd1b8c5b419816a4184
Dmitry Orlov <me@mosquito.su>
2021-02-16 17:59:57 +0300

    fix assets

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -88,7 +88,7 @@ release = ".".join(map(str, module.version_info))
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-#language = None
+language = None
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
