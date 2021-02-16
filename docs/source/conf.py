6730b1ad39a1c8e9b14270598c73a121f6873ebf
Dmitry Orlov <me@mosquito.su>
2021-02-16 17:47:54 +0300

    fix assets

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -88,7 +88,7 @@ release = ".".join(map(str, module.version_info))
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-language = None
+#language = None
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
@@ -122,7 +122,7 @@ html_theme_options = {
     "github_repo": "aiomisc",
     "github_type": "star",
     "github_user": "aiokitchen",
-    "travis_button": True,
+    "travis_button": False,
 }
 
 
