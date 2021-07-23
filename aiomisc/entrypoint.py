cfefa546b1f8ddd56d2281fb3eec7fa0b0cb5ba0
Dmitry Orlov <me@mosquito.su>
2021-07-24 00:35:51 +0300

    add type hint

--- aiomisc/entrypoint.py
+++ aiomisc/entrypoint.py
@@ -92,7 +92,7 @@ class Entrypoint:
         self.shutting_down = False
         self.pre_start = self.PRE_START.copy()
         self.post_stop = self.POST_STOP.copy()
-        self._tasks = WeakSet()
+        self._tasks: t.MutableSet[asyncio.Task] = WeakSet()
 
         if self.log_config:
             basic_config(
