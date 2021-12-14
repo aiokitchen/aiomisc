b84d5ab0f024d94a9378bc19abe16894539dc662
Nikitin Artyom <tzoiker@gmail.com>
2021-12-14 21:01:48 +0300

    fix: session agen fixtures (#128)
    Co-authored-by: Artyom Nikitin <a.nikitin@edadeal.ru>

---
+++ tests/pytest_plugin/test_agen_fixture.py
@@ -0,0 +1,21 @@
+import pytest
+
+from aiomisc import entrypoint
+
+
+class TestSessionScopeAsyncGenFixture:
+
+    @pytest.fixture(scope='session')
+    def loop(self):
+        with entrypoint() as loop:
+            yield loop
+
+    @pytest.fixture(scope='session')
+    async def fixture(self):
+        yield
+
+    async def test_using_fixture(self, fixture):
+        pass
+
+    async def test_not_using_fixture(self):
+        pass
