b44f42880182657c879ac5ecc5fd93e21741398d
Dmitry Orlov <me@mosquito.su>
2022-03-09 17:13:41 +0300

    fix tests

--- examples/rpc/tests.py
+++ examples/rpc/tests.py
@@ -8,11 +8,6 @@ from rpc.server import RPCServer
 
 
 @pytest.fixture
-def server_port(aiomisc_unused_port):
-    return aiomisc_unused_port
-
-
-@pytest.fixture
 def handlers():
     return {
         "foo": lambda: "bar",
@@ -43,22 +38,22 @@ def services(rpc_server, rpc_client):
     return [rpc_server, rpc_client]
 
 
-async def test_foo(rpc_client):
+async def test_foo(rpc_client: RPCClient):
     assert await rpc_client("foo") == "bar"
 
 
-async def test_multiply(rpc_client):
+async def test_multiply(rpc_client: RPCClient):
     assert await rpc_client("mul", a=1, b=3) == 3
 
 
-async def test_division(rpc_client):
+async def test_division(rpc_client: RPCClient):
     with pytest.raises(Exception):
         assert await rpc_client("div", a=1, b=0)
 
     assert await rpc_client("div", a=10, b=5) == 2.
 
 
-async def test_many(rpc_client):
+async def test_many(rpc_client: RPCClient):
     calls = []
     expected = []
 
@@ -74,7 +69,8 @@ async def test_many(rpc_client):
     assert results == expected
 
 
-async def test_two_way(rpc_client, rpc_server):
+async def test_two_way(rpc_client: RPCClient,
+                       rpc_server: RPCServer):
     assert (
         await rpc_server("div", a=100, b=10) ==
         await rpc_client("div", a=100, b=10)
