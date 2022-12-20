41ec8780fba5317967e91baa458a98ecddb8b98c
Dmitry Orlov <me@mosquito.su>
2022-12-20 19:15:32 +0300

    add __all__ for version

--- aiomisc/version.py
+++ aiomisc/version.py
@@ -3,3 +3,9 @@ import pkg_resources
 
 __version__ = pkg_resources.get_distribution("aiomisc").version
 version_info = tuple(map(int, __version__.split(".")))
+
+
+__all__ = (
+    "__version__",
+    "version_info",
+)
