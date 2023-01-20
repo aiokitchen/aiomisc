0e0ac627bb6e03febab7e1b5095bb51fc2fcccc8
Dmitry Orlov <me@mosquito.su>
2023-01-20 13:54:43 +0300

    Add translations

--- tests/test_io.py
+++ tests/test_io.py
@@ -8,11 +8,17 @@ from typing import List
 import pytest
 
 import aiomisc
-from aiomisc.io import AsyncTextFileIO
+from aiomisc.io import AsyncTextIO
 from tests import unix_only
 
 
 pytestmark = pytest.mark.catch_loop_exceptions
+
+
+compressors = pytest.mark.parametrize(
+    "compression", list(aiomisc.io.Compression),
+    ids=list(map(lambda c: c.name.lower(), aiomisc.io.Compression)),
+)
 
 
 async def test_simple(loop, tmp_path):
@@ -71,7 +77,7 @@ async def test_ordering(loop, tmp_path):
 
 async def test_async_for(loop, tmp_path):
     tdir = Path(tmp_path)
-    afp: AsyncTextFileIO
+    afp: AsyncTextIO
 
     async with aiomisc.io.async_open(tdir / "path", "w", loop=loop) as afp:
         await afp.write("foo\nbar\nbaz\n")
@@ -149,3 +155,22 @@ async def test_object(loop):
                 assert isinstance(afp.errors, str)
                 assert isinstance(afp.line_buffering, bool)
                 assert afp.newlines is None
+
+
+@compressors
+async def test_compression(compression: aiomisc.io.Compression, tmp_path):
+    fname = Path(tmp_path) / "test.file"
+
+    afp: aiomisc.io.AsyncTextIOType
+
+    async with aiomisc.io.async_open(
+        fname, "w", compression=compression,
+    ) as afp:
+        for i in range(1000):
+            await afp.write(f"{i}\n")
+
+    async with aiomisc.io.async_open(
+        fname, "r", compression=compression,
+    ) as afp:
+        for i in range(1000):
+            assert await afp.readline() == f"{i}\n"
