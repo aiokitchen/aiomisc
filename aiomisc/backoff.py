f6c162ef78d33dfd2b0207c833453373484c1110
Dmitry Orlov <me@mosquito.su>
2025-07-29 00:48:26 +0200

    Bckoff is class

--- aiomisc/backoff.py
+++ aiomisc/backoff.py
@@ -1,18 +1,12 @@
 import asyncio
-import sys
+
 from functools import wraps
 from typing import (
-    Any, Callable, Coroutine, Optional, Tuple, Type, TypeVar, Union,
+    Any, Callable, Coroutine, Optional, Tuple, Type, TypeVar, Union, Generic,
+    ParamSpec
 )
 
 from .counters import Statistic
-from .timeout import timeout
-
-
-if sys.version_info >= (3, 10):
-    from typing import ParamSpec
-else:
-    from typing_extensions import ParamSpec
 
 
 Number = Union[int, float]
@@ -30,6 +24,179 @@ class BackoffStatistic(Statistic):
 
 class RetryStatistic(BackoffStatistic):
     pass
+
+
+class Backoff:
+    __slots__ = (
+        "attempt_timeout",
+        "deadline",
+        "pause",
+        "max_tries",
+        "giveup",
+        "exceptions",
+        "statistic",
+    )
+
+    def __init__(
+        self,
+        attempt_timeout: Optional[Number],
+        deadline: Optional[Number],
+        pause: Number = 0,
+        exceptions: Tuple[Type[Exception], ...] = (),
+        max_tries: Optional[int] = None,
+        giveup: Optional[Callable[[Exception], bool]] = None,
+        statistic_name: Optional[str] = None,
+        statistic_class: Type[BackoffStatistic] = BackoffStatistic
+    ):
+        if not pause:
+            pause = 0
+        elif pause < 0:
+            raise ValueError("'pause' must be positive")
+
+        if attempt_timeout is not None and attempt_timeout < 0:
+            raise ValueError("'attempt_timeout' must be positive or None")
+
+        if deadline is not None and deadline < 0:
+            raise ValueError("'deadline' must be positive or None")
+
+        if max_tries is not None and max_tries < 1:
+            raise ValueError("'max_retries' must be >= 1 or None")
+
+        if giveup is not None and not callable(giveup):
+            raise ValueError("'giveup' must be a callable or None")
+
+        exceptions = tuple(exceptions) or ()
+        exceptions += asyncio.TimeoutError,
+
+        self.attempt_timeout = attempt_timeout
+        self.deadline = deadline
+        self.pause = pause
+        self.max_tries = max_tries
+        self.giveup = giveup
+        self.exceptions = exceptions
+        self.statistic = statistic_class(statistic_name)
+
+    def prepare(
+        self,
+        func: Callable[P, Coroutine[Any, Any, T]]
+    ) -> "BackoffExecution[P, T]":
+        return BackoffExecution(
+            function=func,
+            statistic=self.statistic,
+            attempt_timeout=self.attempt_timeout,
+            deadline=self.deadline,
+            pause=self.pause,
+            max_tries=self.max_tries,
+            giveup=self.giveup,
+            exceptions=self.exceptions,
+        )
+
+    async def execute(
+        self,
+        func: Callable[P, Coroutine[Any, Any, T]],
+        *args: P.args,
+        **kwargs: P.kwargs
+    ) -> T:
+        execution = self.prepare(func)
+        return await execution(*args, **kwargs)
+
+    def __call__(
+        self,
+        func: Callable[P, Coroutine[Any, Any, T]]
+    ) -> Callable[P, Coroutine[Any, Any, T]]:
+        if not asyncio.iscoroutinefunction(func):
+            raise TypeError("Function must be a coroutine function")
+
+        @wraps(func)
+        async def wrapper(*args: P.args, **kwargs: P.kwargs) -> T:
+            return await self.execute(func, *args, **kwargs)
+
+        return wrapper
+
+
+class BackoffExecution(Generic[P, T]):
+    __slots__ = (
+        "attempt_timeout",
+        "deadline",
+        "exceptions",
+        "function",
+        "giveup",
+        "last_exception",
+        "max_tries",
+        "pause",
+        "statistic",
+        "total_tries",
+    )
+
+    def __init__(
+        self,
+        function: Callable[P, Coroutine[Any, Any, T]],
+        statistic: BackoffStatistic,
+        attempt_timeout: Optional[Number],
+        deadline: Optional[Number],
+        pause: Number = 0,
+        exceptions: Tuple[Type[Exception], ...] = (),
+        max_tries: Optional[int] = None,
+        giveup: Optional[Callable[[Exception], bool]] = None,
+    ):
+        self.function = function
+        self.statistic = statistic
+        self.attempt_timeout = attempt_timeout
+        self.deadline = deadline
+        self.pause = pause
+        self.max_tries = max_tries
+        self.giveup = giveup
+        self.exceptions = exceptions
+
+        self.last_exception: Optional[Exception] = None
+        self.total_tries: int = 0
+
+    async def __call__(self, *args: P.args, **kwargs: P.kwargs) -> T:
+        return await self.execute(*args, **kwargs)
+
+    async def execute(self, *args: P.args, **kwargs: P.kwargs) -> T:
+        async def run() -> Any:
+            loop = asyncio.get_running_loop()
+
+            while True:
+                self.statistic.attempts += 1
+                self.total_tries += 1
+                delta = -loop.time()
+
+                try:
+                    return await asyncio.wait_for(
+                        self.function(*args, **kwargs),
+                        timeout=self.attempt_timeout,
+                    )
+                except asyncio.CancelledError:
+                    self.statistic.cancels += 1
+                    raise
+                except self.exceptions as e:
+                    self.statistic.errors += 1
+                    self.last_exception = e
+                    if (
+                        self.max_tries is not None and
+                        self.total_tries >= self.max_tries
+                    ):
+                        raise
+
+                    if self.giveup and self.giveup(e):
+                        raise
+                    await asyncio.sleep(self.pause)
+                except Exception as e:
+                    self.last_exception = e
+                    raise
+                finally:
+                    delta += loop.time()
+                    self.statistic.sum_time += delta
+                    self.statistic.done += 1
+
+        try:
+            return await asyncio.wait_for(run(), timeout=self.deadline)
+        except Exception:
+            if self.last_exception is not None:
+                raise self.last_exception
+            raise
 
 
 # noinspection SpellCheckingInspection
@@ -58,90 +225,23 @@ def asyncbackoff(
                             execution attempt.
     :param deadline: is maximum execution time for all execution attempts.
     :param pause: is time gap between execution attempts.
-    :param exc: retrying when this exceptions was raised.
+    :param exc: retrying when these exceptions were raised.
     :param exceptions: similar as exc but keyword only.
     :param max_tries: is maximum count of execution attempts (>= 1).
     :param giveup: is a predicate function which can decide by a given
     :param statistic_class: statistic class
     """
 
-    exceptions = exc + tuple(exceptions)
-    statistic = statistic_class(statistic_name)
-
-    if not pause:
-        pause = 0
-    elif pause < 0:
-        raise ValueError("'pause' must be positive")
-
-    if attempt_timeout is not None and attempt_timeout < 0:
-        raise ValueError("'attempt_timeout' must be positive or None")
-
-    if deadline is not None and deadline < 0:
-        raise ValueError("'deadline' must be positive or None")
-
-    if max_tries is not None and max_tries < 1:
-        raise ValueError("'max_retries' must be >= 1 or None")
-
-    if giveup is not None and not callable(giveup):
-        raise ValueError("'giveup' must be a callable or None")
-
-    exceptions = tuple(exceptions) or ()
-    exceptions += asyncio.TimeoutError,
-
-    def decorator(
-        func: Callable[P, Coroutine[Any, Any, T]],
-    ) -> Callable[P, Coroutine[Any, Any, T]]:
-        if attempt_timeout is not None:
-            func = timeout(attempt_timeout)(func)
-
-        @wraps(func)
-        async def wrap(*args: P.args, **kwargs: P.kwargs) -> T:
-            last_exc = None
-            tries = 0
-
-            async def run() -> Any:
-                nonlocal last_exc, tries
-
-                loop = asyncio.get_running_loop()
-
-                while True:
-                    statistic.attempts += 1
-                    tries += 1
-                    delta = -loop.time()
-
-                    try:
-                        return await asyncio.wait_for(
-                            func(*args, **kwargs),
-                            timeout=attempt_timeout,
-                        )
-                    except asyncio.CancelledError:
-                        statistic.cancels += 1
-                        raise
-                    except exceptions as e:
-                        statistic.errors += 1
-                        last_exc = e
-                        if max_tries is not None and tries >= max_tries:
-                            raise
-                        if giveup and giveup(e):
-                            raise
-                        await asyncio.sleep(pause)
-                    except Exception as e:
-                        last_exc = e
-                        raise
-                    finally:
-                        delta += loop.time()
-                        statistic.sum_time += delta
-                        statistic.done += 1
-
-            try:
-                return await asyncio.wait_for(run(), timeout=deadline)
-            except Exception:
-                if last_exc:
-                    raise last_exc
-                raise
-
-        return wrap
-    return decorator
+    return Backoff(
+        attempt_timeout=attempt_timeout,
+        deadline=deadline,
+        pause=pause,
+        exceptions=exceptions or exc,
+        max_tries=max_tries,
+        giveup=giveup,
+        statistic_name=statistic_name,
+        statistic_class=statistic_class,
+    )
 
 
 def asyncretry(
--- aiomisc/timeout.py
+++ aiomisc/timeout.py
@@ -1,13 +1,6 @@
 import asyncio
-import sys
 from functools import wraps
-from typing import Any, Callable, Coroutine, TypeVar, Union
-
-
-if sys.version_info >= (3, 10):
-    from typing import ParamSpec
-else:
-    from typing_extensions import ParamSpec
+from typing import Any, Callable, Coroutine, TypeVar, Union, ParamSpec
 
 
 T = TypeVar("T")
